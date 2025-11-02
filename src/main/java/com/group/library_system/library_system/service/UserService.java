f48de0424f515c55d81a3b11efbc11f179a586ae
mene0903 <cude0903@naver.com>
2025-11-02 21:01:06 +0900

    register and login

--- src/main/java/com/group/library_system/library_system/service/UserService.java
+++ src/main/java/com/group/library_system/library_system/service/UserService.java
@@ -6,6 +6,8 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 
 @Service
 public class UserService {
@@ -26,6 +28,23 @@ public class UserService {
         }
 
         userRepository.save(user);  //예외가 발생하지 않으면 회원 정보 저장
+    }
+
+    public User login(String id, String password) {
+        Optional<User> ID = userRepository.findById(id);
+
+        if (ID.isEmpty()) {
+            // 아이디가 존재하지 않으면 예외 발생
+            throw new IllegalArgumentException("아이디가 존재하지 않습니다.");
+        }
+
+        User user = ID.get();
+
+        if (!user.getPassword().equals(password)) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        return user;
     }
 
 }
