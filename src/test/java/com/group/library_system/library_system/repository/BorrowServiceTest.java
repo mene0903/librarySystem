d63ff9f247d27748e7a7d7dd8eeebff91801a76d
mene0903 <cude0903@naver.com>
2025-11-11 13:51:23 +0900

    book borrow

---
+++ src/test/java/com/group/library_system/library_system/repository/BorrowServiceTest.java
@@ -0,0 +1,64 @@
+package com.group.library_system.library_system.repository;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.group.library_system.library_system.service.BookService;
+import com.group.library_system.library_system.service.BorrowService;
+import com.group.library_system.library_system.service.UserService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.jayway.jsonpath.internal.path.PathCompiler.fail;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+@Transactional
+public class BorrowServiceTest {
+
+    @Autowired
+    UserService userService;
+    @Autowired
+    BorrowRepository borrowRepository;
+    @Autowired
+    BorrowService borrowService;
+
+    User createTestUser(String id) {
+        return new User(null, "Kim", id, "eee", null, "010-0000", null);
+    }
+
+    @Test
+    @DisplayName("borrow 성공")
+    void borrowSuccess() throws JsonProcessingException {
+        //given
+        User user = createTestUser("123");
+        //when
+        userService.registerUser(user);
+        borrowService.saveInfo(user.getId(), "9788937460777");
+        //then
+        Assertions.assertThat(borrowRepository.existsByBookIsbn("9788937460777")).isTrue();
+    }
+
+    @Test
+    @DisplayName("borrow 실패")
+    void borrowFail() throws JsonProcessingException {
+        User user1 = createTestUser("123");
+        User user2 = createTestUser("321");
+
+        userService.registerUser(user1);
+        userService.registerUser(user2);
+
+        String isbn = "9788937460777";
+
+        borrowService.saveInfo(user1.getId(), isbn);
+
+        try {
+            borrowService.saveInfo(user2.getId(), "9788937460777"); // 두 번째 저장 시도
+            fail("예외가 발생해야 합니다."); // 예외 안 나오면 테스트 실패
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage()).isEqualTo("이미 대출이 되어있는 책입니다.");
+        }
+    }
+}
